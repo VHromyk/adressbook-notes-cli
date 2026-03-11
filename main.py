dab7335f5b970552634604dd6aab4b08e4146b90
Mariia <mariyabugaeva1409@gmail.com>
2026-03-11 11:59:05 +0200

    notes fix

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from help_commands import help_info
 from contacts import add_contact, delete_contact, show_all, search_contact, AddressBook
 from commands_enum import Command
 from storage import load_data, save_data
+from notes.notes import NotesRecord
 
 ADDRESS_BOOK_DATA_FILE = "addressbook.pkl"
 
 
 def main():
     print("Welcome to Personal Assistant!")
+    notes = NotesRecord()
 
     try:
         book = load_data(ADDRESS_BOOK_DATA_FILE)
@@ -41,6 +43,27 @@ def main():
         elif command == Command.HELP.value:
             help_info()
 
+        elif command == Command.ADD_NOTE.value:
+            notes.add_note()
+
+        elif command == Command.DELETE_NOTE.value:
+            notes.delete_note() 
+
+        elif command == Command.EDIT_NOTE_TITLE.value:
+            notes.edit_title()   
+
+        elif command == Command.EDIT_NOTE_DESCRIPTION.value:
+            notes.edit_description()    
+
+        elif command == Command.EDIT_NOTE_TAG.value:
+            notes.edit_tag()  
+
+        elif command == Command.SEARCH_NOTE.value:
+            notes.search_note()   
+
+        elif command == Command.SHOW_NOTES.value:
+            notes.show_notes()  
+            
         else:
             print("Invalid command.")
 
--- notes/notes.py
+++ notes/notes.py
@@ -33,51 +33,50 @@ class NotesRecord:
                 line = f"{note['title']} | {note['description']} | {note['tag']}\n"
                 file.write(line)
 
-    def add_title(self, title):
-        self.notes.append({"title": title, "description": "", "tag": ""})
+    def add_note(self):
+        title = input("Enter title: ")
+        description = input("Enter description: ")
+        tag = input("Enter tag: ")
+
+        self.notes.append({
+                "title": title,
+                "description": description,
+                "tag": tag
+            })
+
         self.save_notes()
 
-    def add_description(self, title, description):
-        for note in self.notes:
-            if note["title"] == title:
-                note["description"] = description
-        self.save_notes()
-
-    def add_tag(self, title, tag=""):
-        for note in self.notes:
-            if note["title"] == title:
-                note["tag"] = tag
-        self.save_notes()
-
-    def delete_note(self, title):
+    def delete_note(self):
+        title = input("Enter title for delete: ")
         self.notes = [note for note in self.notes if note["title"] != title]
         self.save_notes()
 
-    def edit_title(self, old_title, new_title):
+    def edit_title(self):
+        old_title = input("Enter old title for edit: ")
+        new_title = input("Enter new title for edit: ")
         for note in self.notes:
             if note["title"] == old_title:
                 note["title"] = new_title
         self.save_notes()
 
-    def edit_description(self, title, new_description):
+    def edit_description(self):
+        title = input("Enter title for edit description: ")
+        new_description = input("Enter new description: ")
         for note in self.notes:
             if note["title"] == title:
                 note["description"] = new_description
         self.save_notes()
 
-    def edit_tag(self, title, new_tag):
+    def edit_tag(self):
+        title = input("Enter title for edit tag: ")
+        new_tag = input("Enter new tag: ")
         for note in self.notes:
             if note["title"] == title:
                 note["tag"] = new_tag
         self.save_notes()
 
-    def delete_tag(self, title):
-        for note in self.notes:
-            if note["title"] == title:
-                note["tag"] = ""
-        self.save_notes()
-
     def search_note(self, word):
+            word = input("Enter word for search note: ")
             results = []
             for note in self.notes:
                 if word.lower() in note["title"].lower() or \
