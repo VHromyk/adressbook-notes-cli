355a95256663a491eda1120b86d9d9c4e6a55ba2
Vitalii Hromyk <hromykk@gmail.com>
2026-03-12 12:23:33 +0200

    fix commands, fix tags

--- help_commands.py
+++
@@ -1,7 +0,0 @@
-def help_info():
-    print('-'*10)
-    print('Commands:')
-    print('help [show this help]')
-    print('hello [say hello]')
-    print('exit [exit the program]')
-    print('-'*10)
